37b162de439554f005da469e0c464c7648c1c966
Eduardo Nunes <esnunes@gmail.com>
2018-11-28 14:32:08 +0100

    Remove babel - use node v10.14 features

--- bin/my-proxy.js
+++ bin/my-proxy.js
@@ -1,13 +1,3 @@
 #!/usr/bin/env node
 
-var path = require('path');
-var escapeRE = require('escape-regexp');
-
-
-require('babel/register')({
-  stage: 0,
-  ignore: new RegExp('^' + escapeRE(path.join(__dirname, '../node_modules'))),
-});
-
-
 require('../lib/cli');
